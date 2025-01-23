c8715d523bd6636ce7c47a12b51af66c8f6aaf00
EduMarsola <eduimarsola@gmail.com>
2025-01-23 20:15:01 -0300

    23/01/2025

--- Date.js
+++ Date.js
@@ -1,10 +1,33 @@
 import { fastify } from 'fastify'
+import mysql from 'mysql2'
 
 const server = fastify()
+//const memory = new Memory()
+
+
+var con = mysql.createConnection({
+    server : 'localhost',
+    port : 5555,
+    user : 'root',
+    password : 11
+})
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("conectado")
+})
+con.connect()
+
+
+
 server.get('/', () => {
-    return Date.now()
+    memory.List()
+    return
+})
+server.post('/', () => {
+    return 
 })
 
 
-server.listen(3000)
+server.listen({ port : 3000})
 
---
+++ memory.js
@@ -0,0 +1,14 @@
+export class Memory{
+    men = new Array()
+
+    Add(object)
+    {
+        this.men.Add(object)
+    }
+    List()
+    {
+        return this.men.values()
+    }
+
+}
+
